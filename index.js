5f38d8f964ea82a86fc48ea1ee4487eb968a6dae
erretres <diegoperezcorreo@gmail.com>
2020-11-02 18:37:06 -0300

    test

--- index.js
+++ index.js
@@ -1,44 +1,88 @@
+const Todo      = require('./models/todo.js');
+const mongoose  = require('mongoose');
+const helmet    = require('helmet');
+const cors      = require('cors');
+const express   = require('express');
+const app       = express();
+
+/* Environment variables */
+
 require('dotenv').config();
-const dbURL         = process.env.DB_URL; 
-const express       = require('express');
-const {v4: uuidv4}  = require('uuid');
-const cors          = require('cors');
-const bodyParser    = require('body-parser');
-const app           = express();
+const DBURI         = process.env.DB_URI;
 const PORT          = process.env.PORT || 5000;
-let   todos         = []; 
+
+/* Data Base */
+
+mongoose.connect(DBURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+
+mongoose.connection.on('connected', () => {
+    console.log('DB connected');
+});
+
+mongoose.connection.on('error', err => {
+    console.log('DB connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('DB disconnected');
+});
+
+mongoose.connection.on('open',  () => {
+    console.log('DB connection is open');
+});
+
+process.on('SIGINT', function() {
+    mongoose.connection.close( () =>  {
+      console.log('Mongoose disconnected on app termination');
+      process.exit(0);
+    });
+});
+
+/*  Middleware */
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(helmet());
 
-app.get('/todo', (req, res)=>{
-    res.json({data: todos});
+/*  Routes */
+
+app.get('/todo', (req, res) => {
+    Todo.find().then(response => res.json({data: response}));
 });
 
-app.post('/todo', (req, res)=>{
-    let newTodo = {id: uuidv4(), data: req.body.data, isCompleted: false};
-    todos.push(newTodo);
-    res.json({timeStamp: Date.now()});
+app.post('/todo', (req, res) => {
+    let newTodo = new Todo({data: req.body.data, isCompleted: req.body.isCompleted});
+    newTodo.save().then(() => res.sendStatus(204));
 });
 
-app.put('/todo', (req, res)=>{
-    todos = todos.map(item => {
-     if(item.id === req.body.data) {
-       return {id: item.id, data: item.data, isCompleted: !item.isCompleted}; 
-     }else{
-       return item;
-     }
-  }); 
-    res.json({timeStamp: Date.now()});
+app.put('/todo', async (req, res, next) => {
+   try {
+    let todo = await Todo.findById({_id: req.body.id});
+    await Todo.updateOne({_id: req.body.id}, {isCompleted: !todo.isCompleted});
+    await res.sendStatus(204);
+    } catch(error) {
+        if(error.message === "Cannot read property \'isCompleted\' of null")  {
+            res.sendStatus(409);
+        }
+
+    }
+
+
 });
 
-app.delete('/todo', (req, res)=>{
-    todos = todos.filter(item => item.id !== req.body.data);
-    res.json({timeStamp: Date.now()});
+app.delete('/todo', async (req, res) => {
+    await Todo.deleteOne({_id: req.body.id});
+    await res.sendStatus(204);
 });
 
-app.listen(PORT, () => {
-    console.log(`Listening at http://localhost:${PORT}`);
+
+app.use(function (err, req, res, next) {
+    console.error('err',   err); // typeError 
+    console.error('stack', err.stack);
+ // res.status(500).send('Something broke!')
 });
 
+
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
